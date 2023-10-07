24f83519c3c4501c87f4002ae69b1207b5468728
james Agu <preagu@yahoo.com>
2023-10-07 02:11:22 +0100

    Created Header and Body components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
+import Body from './components/Body';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      My Project
+      <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Body() {
+  return (
+    <div>
+      <h3>Body</h3>
+    </div>
+  )
+}
